b18e15daf6187b26870c7bfacb1ecc6e62341b70
Athlon1600 <Athlon2400@gmail.com>
2019-11-17 14:49:42 -0600

    include weekly stats too

--- vnstat.php
+++ vnstat.php
@@ -2,6 +2,9 @@
 
 header("Content-Type: text/plain");
 
+exec("vnstat -w", $output, $return_var);
+
+//  Note that if the array already contains some elements, exec() will append to the end of the array.
 exec("vnstat -d", $output, $return_var);
 
 echo implode("\r\n", $output);
