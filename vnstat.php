89f84663918ff639581a69586c54f592f30ddda1
Athlon1600 <Athlon2400@gmail.com>
2020-02-28 19:01:16 -0600

    Update vnstat.php

--- vnstat.php
+++ vnstat.php
@@ -4,6 +4,11 @@ header("Content-Type: text/plain");
 
 exec("vnstat -w", $output, $return_var);
 
+// http://tldp.org/LDP/abs/html/exitcodes.html
+if ($return_var == 127) {
+    die("Error: vnstat not installed!");
+}
+
 //  Note that if the array already contains some elements, exec() will append to the end of the array.
 exec("vnstat -d", $output, $return_var);
 
